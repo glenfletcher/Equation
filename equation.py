4ae9493512d6b96b9c5fc484a0cdd79f6292f82e
Glen Fletcher <glenflet@gmail.com>
2014-03-14 14:21:59 +1100

    Add Plugin Loading to Equation.py

--- equation.py
+++ equation.py
@@ -1,5 +1,11 @@
 import numpy as np
 import re
+import glob
+import os
+import os.path
+import sys
+import traceback
+import importlib
 
 class ExpressionObject (object):
     def __init__(self,expression,*args,**kwargs):
@@ -85,30 +91,27 @@ class ExpressionVariable( ExpressionObject ):
         
     def __repr__(self):
         return "<{0:s}.{1:s}({2:s}) object at {3:0=#10x}>".format(type(self).__module__,type(self).__name__,str(self.name),id(self))
+            
+def addFn(id,str,args,func):
+    global fmatch
+    sys.modules[__name__].functions[id] = {
+        'str': str,
+        'args': args,
+        'prec': 1,
+        'type': 'FUNC',
+        'func': func}
 
-constants = {"pi": np.pi, "e":np.e, "Inf":np.Inf, "NaN":np.NaN}
-functions = {
-            '+':{'str':"({0:s} + {1:s})",'args':2,'prec':1,'type':'LEFT','func':np.add},
-            '-':{'str':"({0:s} - {1:s})",'args':2,'prec':1,'type':'LEFT','func':np.subtract},
-            '*':{'str':"({0:s} * {1:s})",'args':2,'prec':2,'type':'LEFT','func':np.multiply},
-            '/':{'str':"({0:s} / {1:s})",'args':2,'prec':2,'type':'LEFT','func':np.divide},
-            '%':{'str':"({0:s} % {1:s})",'args':2,'prec':2,'type':'LEFT','func':np.mod},
-            '^':{'str':"({0:s} ^ {1:s})",'args':2,'prec':3,'type':'LEFT','func':np.power},
-            '&':{'str':"({0:s} & {1:s})",'args':2,'prec':1,'type':'LEFT','func':np.logical_and},
-            '|':{'str':"({0:s} | {1:s})",'args':2,'prec':1,'type':'LEFT','func':np.logical_or},
-            '!':{'str':"(!{0:s})",'args':1,'prec':0,'type':'RIGHT','func':np.logical_not},
-            'abs':{'str':"abs({0:s})",'args':1,'prec':1,'type':'FUNC','func':np.abs},     
-            'sin':{'str':"sin({0:s})",'args':1,'prec':1,'type':'FUNC','func':np.sin},
-            'cos':{'str':"cos({0:s})",'args':1,'prec':1,'type':'FUNC','func':np.cos},
-            'tan':{'str':"tan({0:s})",'args':1,'prec':1,'type':'FUNC','func':np.tan},
-            're':{'str':"re({0:s})",'args':1,'prec':1,'type':'FUNC','func':np.real},
-            'im':{'str':"im({0:s})",'args':1,'prec':1,'type':'FUNC','func':np.imag},
-        }
-        
-smatch = re.compile("\s*,")
-vmatch = re.compile("\s*(?P<rvalue>[+-]?(?:\d+\.\d+|\d+\.|\.\d+|\d+))(?:[Ee](?P<rexpoent>[+-]\d+))?(?:\s*(?P<sep>\+)?\s*(?P<ivalue>(?(rvalue)(?(sep)[+-]?|[+-])|[+-]?)?(?:\d+\.\d+|\d+\.|\.\d+|\d+))(?:[Ee](?P<iexpoent>[+-]\d+))?[ij])?")
-nmatch = re.compile("\s*([a-zA-Z_][a-zA-Z0-9_]*)")
-fmatch = re.compile('\s*(\(|\)|' + '|'.join(map(re.escape,functions.keys())) + ')')
+def addOp(id,str,single,prec,left,func):
+    global fmatch
+    sys.modules[__name__].functions[id] = {
+        'str': str,
+        'args': 1 if single else 2,
+        'prec': prec,
+        'type': 'LEFT' if left else 'RIGHT',
+        'func': func}
+
+def addConst(name,value):
+    sys.modules[__name__].constants[name] = value
 
 class Expression( object ):
     
@@ -118,7 +121,7 @@ class Expression( object ):
         self.__compile()
 
     def __call__(self,**variables):
-        self.variables = constants # i.e. pi, e, i, etc.
+        self.variables = sys.modules[__name__].constants # i.e. pi, e, i, etc.
         self.variables.update(variables)
         expr = self.__expr[::-1]
         args = [];
@@ -132,22 +135,22 @@ class Expression( object ):
             return args[0]
         
     def __next(self):
-        m = fmatch.match(self.__expression)
+        m = sys.modules[__name__].fmatch.match(self.__expression)
         if m != None:
             self.__expression = self.__expression[m.end():]
             g = m.groups()
             return g[0]
-        m = nmatch.match(self.__expression)
+        m = sys.modules[__name__].nmatch.match(self.__expression)
         if m != None:
             self.__expression = self.__expression[m.end():]
             g = m.groups()
             return g[0]
-        m = vmatch.match(self.__expression)
+        m = sys.modules[__name__].vmatch.match(self.__expression)
         if m != None:
             self.__expression = self.__expression[m.end():]
             g = m.groupdict(0)
             return np.complex(float(g["rvalue"])*10**float(g["rexpoent"]),float(g["ivalue"])*10**float(g["iexpoent"]))
-        m = smatch.match(self.__expression)
+        m = sys.modules[__name__].smatch.match(self.__expression)
         if m != None:
             self.__expression = self.__expression[m.end():]
             return ","
@@ -192,12 +195,12 @@ class Expression( object ):
             elif v == ")":
                 op = stack.pop()
                 while op != "(":
-                    fs = functions[op]
+                    fs = sys.modules[__name__].functions[op]
                     self.__expr.append(ExpressionFunction(fs['func'],fs['args'],fs['str'],op,False,self))
                     op = stack.pop()
-                if len(stack) > 0 and stack[-1] in functions and functions[stack[-1]]['type'] == 'FUNC':
+                if len(stack) > 0 and stack[-1] in sys.modules[__name__].functions and sys.modules[__name__].functions[stack[-1]]['type'] == 'FUNC':
                     op = stack.pop()
-                    fs = functions[op]
+                    fs = sys.modules[__name__].functions[op]
                     args = argc.pop()
                     if fs['args'] != '+' or (args != fs['args'] and args not in fs['args']):
                         self.__expr.append(ExpressionFunction(fs['func'],args,fs['str'],op,True,self))
@@ -205,12 +208,12 @@ class Expression( object ):
                 argc[-1] += 1
                 op = stack.pop()
                 while op != "(":
-                    fs = functions[op]
+                    fs = sys.modules[__name__].functions[op]
                     self.__expr.append(ExpressionFunction(fs['func'],fs['args'],fs['str'],op,False,self))
                     op = stack.pop()
                 stack.append(op)
-            elif v in functions:
-                fn = functions[v]
+            elif v in sys.modules[__name__].functions:
+                fn = sys.modules[__name__].functions[v]
                 if fn['type'] == 'FUNC':
                     stack.append(v)
                     argc.append(1)
@@ -225,7 +228,7 @@ class Expression( object ):
                         stack.append(v)
                         v = self.__next()
                         continue
-                    fs = functions[op]
+                    fs = sys.modules[__name__].functions[op]
                     while True:
                         if (fn['type'] == 'LEFT' and fn['prec'] == fs['prec']) or (fn['prec'] < fs['prec']):
                             self.__expr.append(ExpressionFunction(fs['func'],fs['args'],fs['str'],op,False,self))
@@ -237,7 +240,7 @@ class Expression( object ):
                                 stack.append(v)
                                 break
                             op = stack.pop()
-                            fs = functions[op]
+                            fs = sys.modules[__name__].functions[op]
                         else:
                             stack.append(op)
                             stack.append(v)
@@ -250,10 +253,47 @@ class Expression( object ):
         if len(stack) > 0:
             op = stack.pop()
             while op != "(":
-                fs = functions[op]
+                fs = sys.modules[__name__].functions[op]
                 self.__expr.append(ExpressionFunction(fs['func'],fs['args'],fs['str'],op,False,self))
                 if len(stack) > 0:
                     op = stack.pop()
                 else:
                     break
-            
+
+def Load():
+    if not hasattr(Load, "loaded"):
+        Load.loaded = False
+    if not Load.loaded:
+        Load.loaded = True
+        sys.modules[__name__].constants = {}
+        sys.modules[__name__].functions = {}
+        sys.modules[__name__].smatch = re.compile("\s*,")
+        sys.modules[__name__].vmatch = re.compile("\s*(?P<rvalue>[+-]?(?:\d+\.\d+|\d+\.|\.\d+|\d+))(?:[Ee](?P<rexpoent>[+-]\d+))?(?:\s*(?P<sep>\+)?\s*(?P<ivalue>(?(rvalue)(?(sep)[+-]?|[+-])|[+-]?)?(?:\d+\.\d+|\d+\.|\.\d+|\d+))(?:[Ee](?P<iexpoent>[+-]\d+))?[ij])?")
+        sys.modules[__name__].nmatch = re.compile("\s*([a-zA-Z_][a-zA-Z0-9_]*)")
+        Plugins = {}
+        PluginsLoaded = {}
+        if __name__ == "__main__":
+            dir = os.getcwd()
+        else:
+            dir = os.path.dirname(__PATH__)
+        sys.path.append(dir)
+        for file in glob.glob(dir + "/equation_*.py"):
+            Pluginfile,extension = os.path.splitext(os.path.basename(file))
+            if (Pluginfile == "__init__"):
+                continue
+            if (((extension == '.py') or (extension == '.pyc')) and (not PluginsLoaded.has_key(Pluginfile))):
+                PluginsLoaded[Pluginfile] = 1
+                try:
+                        PluginScript = importlib.import_module(Pluginfile)
+                except:
+                        errtype, errinfo, errtrace = sys.exc_info()
+                        fulltrace = ''.join(traceback.format_exception(errtype, errinfo, errtrace)[1:])
+                        print "Was unable to load {0:s}: {1:s}\nTraceback:\n{2:s}".format(Pluginfile, errinfo, fulltrace)
+                        continue
+                if not hasattr(PluginScript,'equation_extend'):
+                    print "The plugin '{0:s}' from file '{1:s}' is invalid because its missing the attribute 'equation_extend'".format(Pluginfile,(dir.rstrip('/') + '/' + Pluginfile + '.' + extension))
+                    continue
+                PluginScript.equation_extend(addOp,addFn,addConst)
+                print "{0:s} Loaded".format(Pluginfile)
+        sys.modules[__name__].fmatch = re.compile('\s*(\(|\)|' + '|'.join(map(re.escape,sys.modules[__name__].functions.keys())) + ')')
+Load()
---
+++ equation_base.py
@@ -0,0 +1,22 @@
+import numpy as np
+
+def equation_extend(addOp,addFn,addConst):
+    addOp('+',"({0:s} + {1:s})",False,1,True,np.add)
+    addOp('-',"({0:s} - {1:s})",False,1,True,np.subtract)
+    addOp('*',"({0:s} * {1:s})",False,2,True,np.multiply)
+    addOp('/',"({0:s} / {1:s})",False,2,True,np.divide)
+    addOp('%',"({0:s} % {1:s})",False,2,True,np.mod)
+    addOp('^',"({0:s} ^ {1:s})",False,3,True,np.power)
+    addOp('&',"({0:s} & {1:s})",False,1,True,np.logical_and)
+    addOp('|',"({0:s} | {1:s})",False,1,True,np.logical_or)
+    addOp('!',"(!{0:s})",True,0,False,np.logical_not)
+    addFn('abs',"abs({0:s})",1,np.abs)
+    addFn('sin',"sin({0:s})",1,np.sin)
+    addFn('cos',"cos({0:s})",1,np.cos)
+    addFn('tan',"tan({0:s})",1,np.tan)
+    addFn('re',"re({0:s})",1,np.real)
+    addFn('im',"im({0:s})",1,np.imag)
+    addConst("pi",np.pi)
+    addConst("e",np.e)
+    addConst("Inf",np.Inf)
+    addConst("NaN",np.NaN)
