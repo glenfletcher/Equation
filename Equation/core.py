Fixed Unary Operators, add Numerical Python Operators

Equation/core.py
@@ -123,13 +123,21 @@ class Expression( object ):
     """
     def __init__(self,expression,argorder=[],*args,**kwargs):
         super(Expression,self).__init__(*args,**kwargs)
-        self.__expression = expression
-        self.__args = argorder;
-        self.__vars = {} # intenral array of preset variables
-        self.__argsused = set()
-        self.__expr = [] # compiled equation tokens
-        self.variables = {} # call variables
-        self.__compile()
+        if isinstance(expression,type(self)): # clone the object
+            self.__args = list(expression.__args)
+            self.__vars = dict(expression.__vars) # intenral array of preset variables
+            self.__argsused = set(expression.__argsused)
+            self.__expr = list(expression.__expr)
+            self.variables = {} # call variables
+        else:
+            self.__expression = expression
+            self.__args = argorder;
+            self.__vars = {} # intenral array of preset variables
+            self.__argsused = set()
+            self.__expr = [] # compiled equation tokens
+            self.variables = {} # call variables
+            self.__compile()
+            del self.__expression
     
     def __getitem__(self, name):
         """fn[var]
@@ -216,27 +224,49 @@ class Expression( object ):
         else:
             return args[0]
         
-    def __next(self):
-        m = fmatch.match(self.__expression)
-        if m != None:
-            self.__expression = self.__expression[m.end():]
-            g = m.groups()
-            return g[0],'FUNC'
-        m = nmatch.match(self.__expression)
-        if m != None:
-            self.__expression = self.__expression[m.end():]
-            g = m.groups()
-            return g[0],'NAME'
-        m = vmatch.match(self.__expression)
-        if m != None:
-            self.__expression = self.__expression[m.end():]
-            g = m.groupdict(0)
-            return np.complex(float(g["rvalue"])*10**float(g["rexpoent"]),float(g["ivalue"])*10**float(g["iexpoent"])),'VALUE'
-        m = smatch.match(self.__expression)
-        if m != None:
-            self.__expression = self.__expression[m.end():]
-            return ",",'SEP'
-        return None
+    def __next(self,__expect_op):
+        if __expect_op:
+            m = gematch.match(self.__expression)    
+            if m != None:
+                self.__expression = self.__expression[m.end():]
+                g = m.groups()
+                return g[0],'CLOSE'
+            m = omatch.match(self.__expression)    
+            if m != None:
+                self.__expression = self.__expression[m.end():]
+                g = m.groups()
+                return g[0],'OP'
+        else:
+            m = gsmatch.match(self.__expression)    
+            if m != None:
+                self.__expression = self.__expression[m.end():]
+                g = m.groups()
+                return g[0],'OPEN'
+            m = umatch.match(self.__expression)
+            if m != None:
+                self.__expression = self.__expression[m.end():]
+                g = m.groups()
+                return g[0],'UNARY'
+            m = fmatch.match(self.__expression)
+            if m != None:
+                self.__expression = self.__expression[m.end():]
+                g = m.groups()
+                return g[0],'FUNC'
+            m = nmatch.match(self.__expression)
+            if m != None:
+                self.__expression = self.__expression[m.end():]
+                g = m.groups()
+                return g[0],'NAME'
+            m = vmatch.match(self.__expression)
+            if m != None:
+                self.__expression = self.__expression[m.end():]
+                g = m.groupdict(0)
+                return np.complex(float(g["rvalue"])*10**float(g["rexpoent"]),float(g["ivalue"])*10**float(g["iexpoent"])),'VALUE'
+            m = smatch.match(self.__expression)
+            if m != None:
+                self.__expression = self.__expression[m.end():]
+                return ",",'SEP'
+            return None
 
     def show(self):
         """Show RPN tokens
@@ -257,16 +287,7 @@ class Expression( object ):
         str
             Latex String respresation of the Expression, suitable for rendering the equation
         """
-        expr = self.__expr[::-1]
-        args = [];
-        while len(expr) > 0:
-            t = expr.pop()
-            r = t.toStr(args)
-            args.append(r)
-        if len(args) > 1:
-            return args
-        else:
-            return args[0]
+        
 
     def __repr__(self):
         """repr(fn)
@@ -289,6 +310,221 @@ class Expression( object ):
             return args
         else:
             return args[0]
+            
+    def __cmp__(self,other):
+        if isinstance(other,Expression):
+            return cmp(repr(self),repr(other))
+        else:
+            raise TypeError("{0:s} is not an {1:s} Object, and can't be compared to an Expression Object".format(repr(other),type(other)))
+    
+    def __combine(self,other,op):  
+        if op not in ops or not isinstance(other,(int,float,complex,type(self),basestring)):
+            return NotImplemented
+        else:
+            obj = type(self)(self)
+            if isinstance(other,(int,float,complex)):
+                obj.__expr.append(ExpressionValue(other,obj))
+            else:
+                if isinstance(other,basestring):
+                    try:
+                        other = type(self)(other)
+                    except:
+                        raise SyntaxError("Can't Convert string, \"{0:s}\" to an Expression Object".format(other))
+                obj.__expr += other.__expr
+                obj.__argsused += other.__argsused
+                for v in other.__args:
+                    if v not in obj.__args:
+                        obj.__args.append(v)
+                for k,v in other.__vars.items():
+                    if k not in obj.__vars:
+                        obj.__vars[k] = v
+                    elif v != obj.__vars[k]:
+                        raise RuntimeError("Predifined Variable Conflict in '{0:s}' two differing values defined".format(k))
+            fn = ops[op]
+            obj.__expr.append(ExpressionFunction(fn['func'],fn['args'],fn['str'],fn['latex'],op,False,obj))
+        return obj
+
+    def __rcombine(self,other,op):
+        if op not in ops or not isinstance(other,(int,float,complex,type(self),basestring)):
+            return NotImplemented
+        else:
+            obj = type(self)(self)
+            if isinstance(other,(int,float,complex)):
+                obj.__expr.insert(0,ExpressionValue(other,obj))
+            else:
+                if isinstance(other,basestring):
+                    try:
+                        other = type(self)(other)
+                    except:
+                        raise SyntaxError("Can't Convert string, \"{0:s}\" to an Expression Object".format(other))
+                obj.__expr = other.__expr + self.__expr
+                obj.__argsused = other.__argsused + self.__expr
+                __args = other.__args
+                for v in obj.__args:
+                    if v not in __args:
+                        __args.append(v)
+                obj.__args = __args
+                for k,v in other.__vars.items():
+                    if k not in obj.__vars:
+                        obj.__vars[k] = v
+                    elif v != obj.__vars[k]:
+                        raise RuntimeError("Predifined Variable Conflict in '{0:s}' two differing values defined".format(k))
+            fn = ops[op]
+            obj.__expr.append(ExpressionFunction(fn['func'],fn['args'],fn['str'],fn['latex'],op,False,obj))
+        return obj
+        
+    def __icombine(self,other,op):  
+        if op not in ops or not isinstance(other,(int,float,complex,type(self),basestring)):
+            return NotImplemented
+        else:
+            obj = self
+            if isinstance(other,(int,float,complex)):
+                obj.__expr.append(ExpressionValue(other,obj))
+            else:
+                if isinstance(other,basestring):
+                    try:
+                        other = type(self)(other)
+                    except:
+                        raise SyntaxError("Can't Convert string, \"{0:s}\" to an Expression Object".format(other))
+                obj.__expr += other.__expr
+                obj.__argsused += other.__argsused
+                for v in other.__args:
+                    if v not in obj.__args:
+                        obj.__args.append(v)
+                for k,v in other.__vars.items():
+                    if k not in obj.__vars:
+                        obj.__vars[k] = v
+                    elif v != obj.__vars[k]:
+                        raise RuntimeError("Predifined Variable Conflict in '{0:s}' two differing values defined".format(k))
+            fn = ops[op]
+            obj.__expr.append(ExpressionFunction(fn['func'],fn['args'],fn['str'],fn['latex'],op,False,obj))
+        return obj
+
+    def __apply(self,op):
+        fn = unary_ops[op]
+        obj = type(self)(self)
+        obj.__expr.append(ExpressionFunction(fn['func'],1,fn['str'],fn['latex'],op,False,obj))
+        return obj
+    
+    def __applycall(self,op):
+        fn = functions[op]
+        if 1 not in fn['args'] or '*' not in fn['args']:
+            raise RuntimeError("Can't Apply {0:s} function, dosen't accept only 1 argument".format(op))
+        obj = type(self)(self)
+        obj.__expr.append(ExpressionFunction(fn['func'],1,fn['str'],fn['latex'],op,False,obj))
+        return obj
+
+    def __add__(self,other):
+        return self.__combine(other,'+')
+    
+    def __sub__(self,other):
+        return self.__combine(other,'-')
+    
+    def __mul__(self,other):
+        return self.__combine(other,'*')
+    
+    def __div__(self,other):
+        return self.__combine(other,'/')
+    
+    def __truediv__(self,other):
+        return self.__combine(other,'/')
+    
+    def __pow__(self,other):
+        return self.__combine(other,'^')  
+    
+    def __mod__(self,other):
+        return self.__combine(other,'%')
+    
+    def __and__(self,other):
+        return self.__combine(other,'&')
+        
+    def __or__(self,other):
+        return self.__combine(other,'|')
+    
+    def __xor__(self,other):
+        return self.__combine(other,'</>')
+    
+    def __radd__(self,other):
+        return self.__rcombine(other,'+')
+    
+    def __rsub__(self,other):
+        return self.__rcombine(other,'-')
+    
+    def __rmul__(self,other):
+        return self.__rcombine(other,'*')
+    
+    def __rdiv__(self,other):
+        return self.__rcombine(other,'/')
+    
+    def __rtruediv__(self,other):
+        return self.__rcombine(other,'/')
+    
+    def __rpow__(self,other):
+        return self.__rcombine(other,'^')  
+    
+    def __rmod__(self,other):
+        return self.__rcombine(other,'%')
+    
+    def __rand__(self,other):
+        return self.__rcombine(other,'&')
+        
+    def __ror__(self,other):
+        return self.__rcombine(other,'|')
+    
+    def __rxor__(self,other):
+        return self.__rcombine(other,'</>')
+        
+    def __iadd__(self,other):
+        return self.__icombine(other,'+')
+    
+    def __isub__(self,other):
+        return self.__icombine(other,'-')
+    
+    def __imul__(self,other):
+        return self.__icombine(other,'*')
+    
+    def __idiv__(self,other):
+        return self.__icombine(other,'/')
+    
+    def __itruediv__(self,other):
+        return self.__icombine(other,'/')
+    
+    def __ipow__(self,other):
+        return self.__icombine(other,'^')  
+    
+    def __imod__(self,other):
+        return self.__icombine(other,'%')
+    
+    def __iand__(self,other):
+        return self.__icombine(other,'&')
+        
+    def __ior__(self,other):
+        return self.__icombine(other,'|')
+    
+    def __ixor__(self,other):
+        return self.__icombine(other,'</>')
+    
+    def __neg__(self):
+        return self.__apply('-')
+    
+    def __invert__(self):
+        return self.__apply('!')
+    
+    def __abs__(self):
+        return self.__applycall('abs')
+    
+    def __getfunction(self,op):
+        if op[1] == 'FUNC':
+            fn = functions[op[0]]
+            fn['type'] = 'FUNC'            
+        elif op[1] == 'UNARY':
+            fn = unary_ops[op[0]]
+            fn['type'] = 'UNARY'
+            fn['args'] = 1
+        elif op[1] == 'OP':
+            fn = ops[op[0]]
+            fn['type'] = 'OP'
+        return fn            
 
     def __compile(self):
         self.__expr = []
@@ -297,28 +533,28 @@ class Expression( object ):
         __expect_op = False
         v = self.__next(__expect_op)
         while v != None:
-            if not __expect_op and v[0] == "(":
+            if not __expect_op and v[1] == "OPEN":
                 stack.append(v)
                 __expect_op = False       
-            elif __expect_op and v[0] == ")":
+            elif __expect_op and v[1] == "CLOSE":
                 op = stack.pop()
-                while op != "(":
-                    fs = functions[op]
-                    self.__expr.append(ExpressionFunction(fs['func'],fs['args'],fs['str'],fs['latex'],op,False,self))
+                while op[1] != "OPEN":
+                    fs = self.__getfunction(op)
+                    self.__expr.append(ExpressionFunction(fs['func'],fs['args'],fs['str'],fs['latex'],op[0],False,self))
                     op = stack.pop()
-                if len(stack) > 0 and stack[-1] in functions and functions[stack[-1]]['type'] == 'FUNC':
+                if len(stack) > 0 and stack[-1] in functions:
                     op = stack.pop()
-                    fs = functions[op]
+                    fs = functions[op[0]]
                     args = argc.pop()
                     if fs['args'] != '+' or (args != fs['args'] and args not in fs['args']):
-                        self.__expr.append(ExpressionFunction(fs['func'],args,fs['str'],fs['latex'],op,True,self))
+                        self.__expr.append(ExpressionFunction(fs['func'],args,fs['str'],fs['latex'],op[0],True,self))
                 __expect_op = True
             elif __expect_op and v[0] == ",":
                 argc[-1] += 1
                 op = stack.pop()
                 while op != "(":
-                    fs = functions[op]
-                    self.__expr.append(ExpressionFunction(fs['func'],fs['args'],fs['str'],fs['latex'],op,False,self))
+                    fs = self.__getfunction(op)
+                    self.__expr.append(ExpressionFunction(fs['func'],fs['args'],fs['str'],fs['latex'],op[0],False,self))
                     op = stack.pop()
                 stack.append(op)
                 __expect_op = False
@@ -334,7 +570,7 @@ class Expression( object ):
                     stack.append(v)
                     v = self.__next(__expect_op)
                     continue
-                fs = functions[op[0]]
+                fs = self.__getfunction(op)
                 while True:
                     if (fn['prec'] <= fs['prec']):
                         self.__expr.append(ExpressionFunction(fs['func'],fs['args'],fs['str'],fs['latex'],op[0],False,self))
@@ -346,7 +582,7 @@ class Expression( object ):
                             stack.append(op)
                             stack.append(v)
                             break                            
-                        fs = functions[op[0]]
+                        fs = self.__getfunction(op)
                     else:
                         stack.append(op)
                         stack.append(v)
@@ -361,13 +597,13 @@ class Expression( object ):
                 argc.append(1)
                 __expect_op = False
             elif not __expect_op and v[1] == 'NAME':
-                self.__argsused.add(v)
+                self.__argsused.add(v[0])
                 if v not in self.__args:
-                    self.__args.append(v)
-                self.__expr.append(ExpressionVariable(v,self))
+                    self.__args.append(v[0])
+                self.__expr.append(ExpressionVariable(v[0],self))
                 __expect_op = True
             elif not __expect_op and v[1] == 'VALUE':
-                self.__expr.append(ExpressionValue(v,self))
+                self.__expr.append(ExpressionValue(v[0],self))
                 __expect_op = True
             else:
                 raise SyntaxError("Invalid Token \"{0:s}\" in Expression, Expected {1:s}".format(v,"Op" if __expect_op else "Value"))
@@ -375,8 +611,8 @@ class Expression( object ):
         if len(stack) > 0:
             op = stack.pop()
             while op != "(":
-                fs = functions[op]
-                self.__expr.append(ExpressionFunction(fs['func'],fs['args'],fs['str'],fs['latex'],op,False,self))
+                fs = self.__getfunction(op)
+                self.__expr.append(ExpressionFunction(fs['func'],fs['args'],fs['str'],fs['latex'],op[0],False,self))
                 if len(stack) > 0:
                     op = stack.pop()
                 else:
@@ -389,8 +625,14 @@ functions = {}
 smatch = re.compile("\s*,")
 vmatch = re.compile("\s*(?P<rvalue>[+-]?(?:\d+\.\d+|\d+\.|\.\d+|\d+))(?:[Ee](?P<rexpoent>[+-]\d+))?(?:\s*(?P<sep>\+)?\s*(?P<ivalue>(?(rvalue)(?(sep)[+-]?|[+-])|[+-]?)?(?:\d+\.\d+|\d+\.|\.\d+|\d+))(?:[Ee](?P<iexpoent>[+-]\d+))?[ij])?")
 nmatch = re.compile("\s*([a-zA-Z_][a-zA-Z0-9_]*)")
-fmatch = re.compile('\s*(\(|\)|' + '|'.join(map(re.escape,functions.keys())) + ')')
+gsmatch = re.compile('\s*(\()')
+gematch = re.compile('\s*(\))')
+fmatch = re.compile('\s*(' + '|'.join(map(re.escape,functions.keys())) + ')')
+omatch = re.compile('\s*(' + '|'.join(map(re.escape,ops.keys())) + ')')
+umatch = re.compile('\s*(' + '|'.join(map(re.escape,unary_ops.keys())) + ')')
 
 def recalculateFMatch():
-    global fmatch
-    fmatch = re.compile('\s*(\(|\)|' + '|'.join(map(re.escape,functions.keys())) + ')')
+    global fmatch, omatch, umatch
+    fmatch = re.compile('\s*(' + '|'.join(map(re.escape,functions.keys())) + ')')
+    omatch = re.compile('\s*(' + '|'.join(map(re.escape,ops.keys())) + ')')
+    umatch = re.compile('\s*(' + '|'.join(map(re.escape,unary_ops.keys())) + ')')