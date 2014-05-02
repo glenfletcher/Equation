52edd384c6594e267f49d730ee183bb78dbc9ac7
Glen Fletcher <glenflet@gmail.com>
2014-05-02 17:20:23 +1000

    Updated constant names, refined and document similar.py

--- Equation/similar.py
+++ Equation/similar.py
@@ -9,6 +9,8 @@
 #       http://www.alphaomega-technology.com.au/license/AOT-OL/1.0
 #==============================================================================
 r"""
+.. _similar-module:
+
 Equation.similar Module
 =======================
 
@@ -19,17 +21,52 @@ Comparsions work by detriming if the following is true
 .. math::
     
     1-\frac{\min(A,B)}{\max(A,B)}\leq tolerance
+    
+If it is true the :math:`A` and :math:`B` are considered to be equal
+
+This module only needs to be imported if you need to change the tolerance for similarlity
+tests, otherwise you just need to use the similarlity operators in your expression
+
+.. code-block:: python
+
+    >>> from Equation import Expression
+    >>> fn = Expression("x ~ y")
+    >>> fn(1,1.1)
+    False
+    >>> fn(1,1.00001)
+    True
+    >>> fn(1,1.001)
+    False
+    >>> from Equation.similar import set_tol
+    >>> set_tol(1e-2)
+    >>> fn(1,1.1)
+    False
+    >>> fn(1,1.00001)
+    True
+    >>> fn(1,1.001)
+    True
+
+By default the tolerance is :math:`10^{-5}` Hence 1.001 isn't condisered similar to 1, but by
+changing the tolerance to :math:`10^{-2}`, 1.001 is condisered similar to 1
 """
 
 _tol = 1e-5
 
 def sim(a,b):
+    if (a==b):
+        return True
+    elif a == 0 or b == 0:
+        return False
     if (a<b):
         return (1-a/b)<=_tol
     else:
         return (1-b/a)<=_tol
 
 def nsim(a,b):
+    if (a==b):
+        return False
+    elif a == 0 or b == 0:
+        return True
     if (a<b):
         return (1-a/b)>_tol
     else:
@@ -49,7 +86,7 @@ def set_tol(value=1e-5):
     r"""Set Error Tolerance
     
     Set the tolerance for detriming if two numbers are simliar, i.e
-    :math:`\frac{a}{b} = 1 \pm tolerance`
+    :math:`\left|\frac{a}{b}\right| = 1 \pm tolerance`
     
     Parameters
     ----------
