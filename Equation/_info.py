56f3f3aa13ec75ca8044839ed9cafc98ca37534e
Glen Fletcher <glenflet@gmail.com>
2014-07-07 12:39:08 +1000

    Update package to use safe setup.py structure, no import

---
+++ Equation/_info.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+#==============================================================================
+#   Copyright 2014 AlphaOmega Technology
+#
+#   Licensed under the AlphaOmega Technology Open License Version 1.0
+#   You may not use this file except in compliance with this License.
+#   You may obtain a copy of the License at
+#
+#       http://www.alphaomega-technology.com.au/license/AOT-OL/1.0
+#==============================================================================
+
+__authors__ = [
+    ("Glen Fletcher", "glen.fletcher@alphaomega-technology.com.au")]
+__copyright__ = "(c) 2014, AlphaOmega Technology"
+__license__ = """\
+AlphaOmega Technology Open License Version 1.0\
+ (http://www.alphaomega-technology.com.au/license/AOT-OL/1.0)"""
+__contact__ = "Glen Fletcher <glen.fletcher@alphaomega-technology.com.au>"
+__version__ = "1.2"
+__title__ = "Equation"
+__desc__ = """\
+General Equation Parser and Evaluator"""
+
+__all__ = [
+        '__authors__', '__copyright__', '__license__',
+        '__contact__', '__version__', '__title__',
+        '__desc__']
--- Equation/util.py
+++ Equation/util.py
@@ -1,15 +1,15 @@
 # -*- coding: utf-8 -*-
 #==============================================================================
 #   Copyright 2014 AlphaOmega Technology
-# 
+#
 #   Licensed under the AlphaOmega Technology Open License Version 1.0
 #   You may not use this file except in compliance with this License.
 #   You may obtain a copy of the License at
-#  
+#
 #       http://www.alphaomega-technology.com.au/license/AOT-OL/1.0
 #==============================================================================
 
-from . import __authors__,__copyright__,__license__,__contact__,__version__
+
 try:
     from Equation import core
 except ImportError:
