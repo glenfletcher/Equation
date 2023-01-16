a0596ef13dc9a7d05033bdf127998d0602b30981
Matthew Scroggs <matthew.w.scroggs@gmail.com>
2023-01-16 12:30:42 +0000

    util

--- Equation/util.py
+++ Equation/util.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-#==============================================================================
+# ==============================================================================
 #   Copyright 2014 AlphaOmega Technology
 #
 #   Licensed under the AlphaOmega Technology Open License Version 1.0
@@ -7,7 +7,7 @@
 #   You may obtain a copy of the License at
 #
 #       http://www.alphaomega-technology.com.au/license/AOT-OL/1.0
-#==============================================================================
+# ==============================================================================
 
 
 try:
@@ -15,30 +15,20 @@ try:
 except ImportError:
     import core
 
-def addFn(id,str,latex,args,func):
-    core.functions[id] = {
-        'str': str,
-        'latex': latex,
-        'args': args,
-        'func': func}
 
-def addOp(id,str,latex,single,prec,func):
+def addFn(id, str, latex, args, func):
+    core.functions[id] = {'str': str, 'latex': latex, 'args': args, 'func': func}
+
+
+def addOp(id, str, latex, single, prec, func):
     if single:
         raise RuntimeError("Single Ops Not Yet Supported")
-    core.ops[id] = {
-        'str': str,
-        'latex': latex,
-        'args': 2,
-        'prec': prec,
-        'func': func}
+    core.ops[id] = {'str': str, 'latex': latex, 'args': 2, 'prec': prec, 'func': func}
 
-def addUnaryOp(id,str,latex,func):
-    core.unary_ops[id] = {
-        'str': str,
-        'latex': latex,
-        'args': 1,
-        'prec': 0,
-        'func': func}
 
-def addConst(name,value):
+def addUnaryOp(id, str, latex, func):
+    core.unary_ops[id] = {'str': str, 'latex': latex, 'args': 1, 'prec': 0, 'func': func}
+
+
+def addConst(name, value):
     core.constants[name] = value
