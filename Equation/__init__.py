a7e41c4d2aabd1c9969a58f15a6c6dd724b6d0c0
Matthew Scroggs <matthew.w.scroggs@gmail.com>
2023-01-16 12:32:05 +0000

    __init__

--- Equation/__init__.py
+++ Equation/__init__.py
@@ -1,4 +1,4 @@
-#==============================================================================
+# ==============================================================================
 #   Copyright 2014 AlphaOmega Technology
 #
 #   Licensed under the AlphaOmega Technology Open License Version 1.0
@@ -6,7 +6,7 @@
 #   You may obtain a copy of the License at
 #
 #       http://www.alphaomega-technology.com.au/license/AOT-OL/1.0
-#==============================================================================
+# ==============================================================================
 """
 Equation Module
 ===============
@@ -26,70 +26,54 @@ should use.
 .. moduleauthor:: Glen Fletcher <glen.fletcher@alphaomega-technology.com.au>
 """
 
+from ._info import (__authors__, __copyright__, __license__,
+                    __contact__, __version__, __title__, __desc__)
+from .core import Expression
 
 all = ['util']
 
-try:
-    from Equation._info import *
-except ImportError:
-    from _info import *
 
-try:
-    from Equation.core import Expression
-except ImportError:
-    from core import Expression
-
-def load():
+def _load():
     import os
     import os.path
     import sys
     import traceback
-    try:
-        import importlib
-    except ImportError:
-        # Python 2.6 dosen't have importlib used dummy object
-        # wrap __import__
-        class importlib():
-            @staticmethod
-            def import_module(name):
-                __import__(name)
-                return sys.modules[name]
-    try:
-        from Equation.core import recalculateFMatch
-    except ImportError:
-        from core import recalculateFMatch
-    if not hasattr(load, "loaded"):
-        load.loaded = False
-    if not load.loaded:
-        load.loaded = True
-        plugins_loaded = {}
-        if __name__ == "__main__":
-            dirname = os.path.abspath(os.getcwd())
-            prefix = ""
-        else:
-            dirname = os.path.dirname(os.path.abspath(__file__))
-            if __package__ != None:
-                prefix = __package__ + "."
-            else:
-                prefix = ""
-        for file in os.listdir(dirname):
-            plugin_file,extension = os.path.splitext(os.path.basename(file))
-            if not plugin_file.lower().startswith("equation_",0,9) or extension.lower() not in ['.py','.pyc']:
-                continue
-            if plugin_file not in plugins_loaded:
-                plugins_loaded[plugin_file] = 1
-                try:
-                        plugin_script = importlib.import_module(prefix + plugin_file)
-                except:
-                        errtype, errinfo, errtrace = sys.exc_info()
-                        fulltrace = ''.join(traceback.format_exception(errtype, errinfo, errtrace)[1:])
-                        sys.stderr.write("Was unable to load {0:s}: {1:s}\nTraceback:\n{2:s}\n".format(plugin_file, errinfo, fulltrace))
-                        continue
-                if not hasattr(plugin_script,'equation_extend'):
-                    sys.stderr.write("The plugin '{0:s}' from file '{1:s}' is invalid because its missing the attribute 'equation_extend'\n".format(plugin_file,(dirname.rstrip('/') + '/' + plugin_file + extension)))
-                    continue
-                plugin_script.equation_extend()
-        recalculateFMatch()
-load()
+    import importlib
+    from .core import recalculateFMatch
 
-del load
+    plugins_loaded = {}
+    if __name__ == "__main__":
+        dirname = os.path.abspath(os.getcwd())
+        prefix = ""
+    else:
+        dirname = os.path.dirname(os.path.abspath(__file__))
+        if __package__ is not None:
+            prefix = __package__ + "."
+        else:
+            prefix = ""
+    for file in os.listdir(dirname):
+        plugin_file, extension = os.path.splitext(os.path.basename(file))
+        if not plugin_file.lower().startswith("equation_", 0, 9) or extension.lower() not in ['.py', '.pyc']:  # noqa: E501
+            continue
+        if plugin_file not in plugins_loaded:
+            plugins_loaded[plugin_file] = 1
+            try:
+                plugin_script = importlib.import_module(prefix + plugin_file)
+            except:  # noqa: E722
+                errtype, errinfo, errtrace = sys.exc_info()
+                fulltrace = ''.join(traceback.format_exception(errtype, errinfo, errtrace)[1:])
+                sys.stderr.write(
+                    f"Was unable to load {plugin_file}: {errinfo}\nTraceback:"
+                    f"\n{fulltrace}\n")
+                continue
+            if not hasattr(plugin_script, 'equation_extend'):
+                sys.stderr.write(
+                    f"The plugin '{plugin_file}' from file "
+                    f"'{dirname.rstrip('/') + '/' + plugin_file + extension}'"
+                    " is invalid because its missing the attribute 'equation_extend'\n")
+                continue
+            plugin_script.equation_extend()
+    recalculateFMatch()
+
+
+_load()
